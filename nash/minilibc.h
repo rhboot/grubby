a16b603bf0a068ed36f6df4485daf5253435a207
Erik Troan <ewt@redhat.com>
2001-01-23 21:23:03 +0000

    first try at nash

---
+++ nash/minilibc.h
@@ -0,0 +1,117 @@
+#include <stdarg.h>
+
+#define _LOOSE_KERNEL_NAMES 1
+
+#define NULL ((void *) 0)
+
+#define	WIFSTOPPED(status)	(((status) & 0xff) == 0x7f)
+#define	WIFSIGNALED(status)	(!WIFSTOPPED(status) && !WIFEXITED(status))
+#define	WEXITSTATUS(status)	(((status) & 0xff00) >> 8)
+#define	WTERMSIG(status)	((status) & 0x7f)
+#define	WSTOPSIG(status)	WEXITSTATUS(status)
+#define	WIFEXITED(status)	(WTERMSIG(status) == 0)
+
+#define MS_MGC_VAL 0xc0ed0000
+
+#define isspace(a) (a == ' ' || a == '\t')
+
+extern char ** _environ;
+
+extern int errno;
+
+/* Aieee, gcc 2.95+ creates a stub for posix_types.h on i386 which brings
+   glibc headers in and thus makes __FD_SET etc. not defined with 2.3+ kernels. */
+#define _FEATURES_H 1
+#include <linux/socket.h>
+#include <linux/types.h>
+#include <linux/time.h>
+#include <linux/if.h>
+#include <linux/un.h>
+#include <linux/loop.h>
+#include <linux/net.h>
+#include <asm/posix_types.h>
+#include <asm/termios.h>
+#include <asm/ioctls.h>
+#include <asm/unistd.h>
+#include <asm/fcntl.h>
+#include <asm/signal.h>
+
+#ifndef MINILIBC_INTERNAL
+static inline _syscall5(int,mount,const char *,spec,const char *,dir,const char *,type,unsigned long,rwflag,const void *,data);
+static inline _syscall5(int,_newselect,int,n,fd_set *,rd,fd_set *,wr,fd_set *,ex,struct timeval *,timeval);
+static inline _syscall4(int,wait4,pid_t,pid,int *,status,int,opts,void *,rusage)
+static inline _syscall3(int,write,int,fd,const char *,buf,unsigned long,count)
+static inline _syscall3(int,reboot,int,magic,int,magic_too,int,flag)
+static inline _syscall3(int,execve,const char *,fn,void *,argv,void *,envp)
+static inline _syscall3(int,read,int,fd,const char *,buf,unsigned long,count)
+static inline _syscall3(int,open,const char *,fn,int,flags,mode_t,mode)
+static inline _syscall3(int,ioctl,int,fd,int,request,void *,argp)
+static inline _syscall2(int,dup2,int,one,int,two)
+static inline _syscall2(int,kill,pid_t,pid,int,sig)
+static inline _syscall2(int,symlink,const char *,a,const char *,b)
+static inline _syscall2(int,chmod,const char * ,path,mode_t,mode)
+static inline _syscall2(int,sethostname,const char *,name,int,len)
+static inline _syscall2(int,setdomainname,const char *,name,int,len)
+static inline _syscall2(int,setpgid,int,name,int,len)
+static inline _syscall2(int,signal,int,num,void *,len)
+static inline _syscall1(int,umount,const char *,dir)
+static inline _syscall1(int,unlink,const char *,fn)
+static inline _syscall1(int,close,int,fd)
+static inline _syscall1(int,swapoff,const char *,fn)
+static inline _syscall0(int,getpid)
+static inline _syscall0(int,getppid)
+static inline _syscall0(int,sync)
+#ifdef __sparc__
+/* Nonstandard fork calling convention :( */
+static inline int fork(void) {
+  int __res;
+  __asm__ __volatile__ (
+    "mov %0, %%g1\n\t"
+    "t 0x10\n\t"
+    "bcc 1f\n\t"
+    "dec %%o1\n\t"
+    "sethi %%hi(%2), %%g1\n\t"
+    "st %%o0, [%%g1 + %%lo(%2)]\n\t"
+    "b 2f\n\t"
+    "mov -1, %0\n\t"
+    "1:\n\t"
+    "and %%o0, %%o1, %0\n\t"
+    "2:\n\t"
+    : "=r" (__res)
+    : "0" (__NR_fork), "i" (&errno)
+    : "g1", "o0", "cc");
+  return __res;
+}
+#else
+static inline _syscall0(int,fork)
+#endif
+static inline _syscall0(pid_t,setsid)
+static inline _syscall3(int,syslog,int, type, char *, buf, int, len);
+#else
+static inline _syscall5(int,_newselect,int,n,fd_set *,rd,fd_set *,wr,fd_set *,ex,struct timeval *,timeval);
+static inline _syscall3(int,write,int,fd,const char *,buf,unsigned long,count)
+static inline _syscall2(int,socketcall,int,code,unsigned long *, args)
+#define __NR__do_exit __NR_exit
+extern inline _syscall1(int,_do_exit,int,exitcode)
+#endif
+
+#define select _newselect
+
+extern int errno;
+
+inline int socket(int a, int b, int c);
+inline int bind(int a, void * b, int c);
+inline int listen(int a, int b);
+inline int accept(int a, void * addr, void * addr2);
+int strlen(const char * string);
+char * strcpy(char * dst, const char * src);
+void * memcpy(void * dst, const void * src, size_t count);
+void * memset(void * dst, int ch, size_t count);
+void sleep(int secs);
+int strcmp(const char * a, const char * b);
+int strncmp(const char * a, const char * b, int len);
+void printint(int i);
+void printf(char * fmt, ...);
+char * strchr(char * str, int ch);
+char * strncpy(char * dst, const char * src, int len);
+void exit(int rc);
---
+++ nash/nash.c
@@ -0,0 +1,375 @@
+/*
+ * linuxrc.c
+ * 
+ * Simple code to load modules, mount root, and get things going. It's designed
+ * not to be linked against libc, which keeps it small.
+ *
+ * Erik Troan (ewt@redhat.com)
+ *
+ * Copyright 2001 Red Hat Software 
+ *
+ * This software may be freely redistributed under the terms of the GNU
+ * public license.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+/* We support losetup, mount, insmod, and echo commands. Comments 
+   and blank lines work as well. */
+
+#if USE_MINILIBC
+#include "minilibc.h"
+#ifndef SOCK_STREAM
+# define SOCK_STREAM 1
+#endif 
+#else
+#include <ctype.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <net/if.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <sys/klog.h>
+#include <sys/mount.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/un.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/ioctl.h>
+#include <sys/reboot.h>
+#include <termios.h>
+
+/* Need to tell loop.h what the actual dev_t type is. */
+#undef dev_t
+#if defined(__alpha) || (defined(__sparc__) && defined(__arch64__))
+#define dev_t unsigned int
+#else
+#define dev_t unsigned short
+#endif
+#include <linux/loop.h>
+#undef dev_t
+#define dev_t dev_t
+
+#define syslog klogctl
+#endif
+
+#include <linux/cdrom.h>
+
+#define MS_REMOUNT      32
+
+#define MAX(a, b) ((a) > (b) ? a : b)
+
+int testing = 0;
+
+char * env[] = {
+    "PATH=/usr/bin:/bin:/sbin:/usr/sbin:/mnt/sysimage/usr/bin:",
+    NULL
+};
+
+char * getArg(char * cmd, char * end, char ** arg) {
+    char quote = '\0';
+
+    if (cmd >= end) return NULL;
+
+    while (isspace(*cmd) && cmd < end) cmd++;
+    if (cmd >= end) return NULL;
+
+    if (*cmd == '"')
+	cmd++, quote = '"';
+    else if (*cmd == '\'')
+	cmd++, quote = '\'';
+
+    if (quote) {
+	*arg = cmd;
+
+	/* This doesn't support \ escapes */
+	while (cmd < end && *cmd != quote) cmd++;
+
+	if (cmd == end) {
+	    printf("error: quote mismatch for %s\n", *arg);
+	    return NULL;
+	}
+
+	*cmd = '\0';
+	cmd++;
+    } else {
+	*arg = cmd;
+	while (!isspace(*cmd) && cmd < end) cmd++;
+	*cmd = '\0';
+    }
+
+    cmd++;
+
+    while (isspace(*cmd)) cmd++;
+
+    return cmd;
+}
+
+void mountCommand(char * cmd, char * end) {
+    char * fsType;
+    char * device;
+    char * mntPoint;
+
+    if (!(cmd = getArg(cmd, end, &fsType)) || strcmp(fsType, "-t")) {
+	printf("mount: -t must be first argument\n");
+	return;
+    }
+
+    if (!(cmd = getArg(cmd, end, &fsType))) {
+	printf("mount: missing filesystem type\n");
+	return;
+    }
+
+    if (!(cmd = getArg(cmd, end, &device))) {
+	printf("mount: missing device\n");
+	return;
+    }
+
+    if (!(cmd = getArg(cmd, end, &mntPoint))) {
+	printf("mount: missing mount point\n");
+	return;
+    }
+
+    if (cmd < end) {
+	printf("mount: unexpected arguments\n");
+	return;
+    }
+
+    if (testing) {
+	printf("mount -t '%s' '%s' '%s'\n", fsType, device, mntPoint);
+    } else {
+	if (mount(device, mntPoint, fsType, MS_MGC_VAL, NULL)) {
+	    printf("mount: error %d mounting %s\n", errno, fsType);
+	}
+    }
+}
+
+void insmodCommand(char * cmd, char * end) {
+    char * args[32];
+    char ** nextArg;
+    int pid;
+    int status;
+
+    nextArg = args;
+    *nextArg = "/sbin/insmod";
+
+    while (cmd && cmd < end) {
+	nextArg++;
+	cmd = getArg(cmd, end, nextArg);
+    }
+	
+    if (cmd) nextArg++;
+    *nextArg = NULL;
+
+    if (testing) {
+	printf("insmod ");
+	nextArg = args + 1;
+	while (*nextArg)
+	    printf(" '%s'", *nextArg++);
+	printf("\n");
+    } else {
+	if (!(pid = fork())) {
+	    /* child */
+	    execve("/sbin/insmod", args, env);
+	    printf("ERROR: failed in exec /sbin/insmod\n");
+	    exit(1);
+	}
+
+	wait4(-1, &status, 0, NULL);
+	if (!WIFEXITED(status) || WEXITSTATUS(status)) {
+	    printf("ERROR: insmod exited abnormally!\n");
+	}
+    }
+}
+
+void losetupCommand(char * cmd, char * end) {
+    char * device;
+    char * file;
+    int fd;
+    struct loop_info loopInfo;
+    int dev;
+
+    if (!(cmd = getArg(cmd, end, &device))) {
+	printf("losetup: missing device\n");
+	return;
+    }
+
+    if (!(cmd = getArg(cmd, end, &file))) {
+	printf("losetup: missing file\n");
+	return;
+    }
+
+    if (cmd < end) {
+	printf("losetup: unexpected arguments\n");
+	return;
+    }
+
+    if (testing) {
+	printf("losetup '%s' '%s'\n", device, file);
+    } else {
+	dev = open(device, O_RDWR, 0);
+	if (dev < 0) {
+	    printf("losetup: failed to open %s: %d\n", device, errno);
+	    return;
+	}
+
+	fd = open(file, O_RDWR, 0);
+	if (fd < 0) {
+	    printf("losetup: failed to open %s: %d\n", file, errno);
+	    close(dev);
+	    return;
+	}
+
+	if (ioctl(dev, LOOP_SET_FD, (void *) fd)) {
+	    printf("losetup: LOOP_SET_FD failed: %d\n", errno);
+	    close(dev);
+	    close(fd);
+	    return;
+	}
+
+	close(fd);
+
+	memset(&loopInfo, 0, sizeof(loopInfo));
+	strcpy(loopInfo.lo_name, file);
+
+	if (ioctl(dev, LOOP_SET_STATUS, &loopInfo)) 
+	    printf("losetup: LOOP_SET_STATUS failed: %d\n", errno);
+
+	close(dev);
+    }
+}
+
+void echoCommand(char * cmd, char * end) {
+    char * args[256];
+    char ** nextArg = args;
+    int outFd = 1;
+    int num = 0;
+    int i;
+
+    if (testing)
+	printf("(echo) ");
+
+    while ((cmd = getArg(cmd, end, nextArg)))
+	nextArg++, num++;
+
+    if ((nextArg - args >= 2) && !strcmp(*(nextArg - 2), ">")) {
+	outFd = open(*(nextArg - 1), O_RDWR | O_CREAT | O_TRUNC, 0644);
+	if (outFd < 0) {
+	    printf("echo: cannot open %s for write: %d\n", 
+		    *(nextArg - 1), errno);
+	    return;
+	}
+
+	num -= 2;
+    }
+
+    for (i = 0; i < num;i ++) {
+	if (i)
+	    write(i, " ", 1);
+	write(outFd, args[i], strlen(args[i]));
+    }
+
+    write(outFd, "\n", 1);
+}
+
+int runStartup(int fd) {
+    char contents[32768];
+    int i;
+    char * start, * end;
+    char * chptr;
+
+    i = read(fd, contents, sizeof(contents) - 1);
+    if (i == (sizeof(contents) - 1)) {
+	printf("Failed to read /startup.rc -- file too large.\n");
+	return 1;
+    }
+
+    contents[i] = '\0';
+
+    start = contents;
+    while (*start) {
+	while (isspace(*start) && *start && (*start != '\n')) start++;
+
+	if (*start == '#')
+	    while (*start && (*start != '\n')) start++;
+
+	if (*start == '\n') {
+	    start++;
+	    continue;
+	}
+
+	if (!*start) {
+	    printf("(last line in /startup.rc is empty)\n");
+	    continue;
+	}
+
+	/* start points to the beginning of the command */
+	end = start + 1;
+	while (*end && (*end != '\n')) end++;
+	if (!*end) {
+	    printf("(last line in /startup.rc missing \\n -- skipping)\n");
+	    start = end;
+	    continue;
+	}
+
+	/* end points to the \n at the end of the command */
+
+	chptr = start;
+	while (chptr < end && !isspace(*chptr)) chptr++;
+
+	if (!strncmp(start, "mount", MAX(5, chptr - start)))
+	    mountCommand(chptr, end);
+	else if (!strncmp(start, "losetup", MAX(7, chptr - start)))
+	    losetupCommand(chptr, end);
+	else if (!strncmp(start, "echo", MAX(4, chptr - start)))
+	    echoCommand(chptr, end);
+	else if (!strncmp(start, "insmod", MAX(6, chptr - start)))
+	    insmodCommand(chptr, end);
+	else {
+	    *chptr = '\0';
+	    printf("Unknown command %s\n", start);
+	}
+
+	start = end + 1;
+    }
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+    int fd = 0;
+    char ** nextArg = argv + 1;
+
+    if (argc > 1 && !strcmp(*nextArg, "--force")) {
+	printf("(forcing normal run)\n");
+	nextArg++;
+    } else
+	testing = (getppid() != 0) && (getppid() != 1);
+
+    if (testing)
+	printf("(running in test mode).\n");
+
+    printf("Red Hat nash version %s starting\n", VERSION);
+
+    if (*nextArg) {
+	fd = open(*nextArg, O_RDONLY, 0);
+	if (fd < 0) {
+	    printf("nash: cannot open %s: %d\n", *nextArg, errno);
+	    exit(1);
+	}
+    }
+
+    runStartup(fd);
+    close(fd);
+
+    return 0;
+}
