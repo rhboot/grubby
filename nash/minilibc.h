bade08e67e38dab6db20e728e7a0ca9633683948
Trond Eivind Glomsrød <teg@redhat.com>
2001-12-18 17:08:48 +0000

    Pull new version from anaconda

--- nash/minilibc.h
+++ nash/minilibc.h
@@ -1,7 +1,5 @@
 #include <stdarg.h>
 
-#include <asm/errno.h>
-
 #define _LOOSE_KERNEL_NAMES 1
 
 #define NULL ((void *) 0)
@@ -12,15 +10,27 @@
 #define	WTERMSIG(status)	((status) & 0x7f)
 #define	WSTOPSIG(status)	WEXITSTATUS(status)
 #define	WIFEXITED(status)	(WTERMSIG(status) == 0)
+#define S_IFBLK			0060000
 
 #define MS_MGC_VAL 0xc0ed0000
-#define MS_RDONLY	 1	/* Mount read-only */
 
 #define isspace(a) (a == ' ' || a == '\t')
 
 extern char ** _environ;
 
 extern int errno;
+
+/* from /usr/include/bits/sigset.h */
+/* A `sigset_t' has a bit for each signal.  */
+
+#define _SIGSET_NWORDS (1024 / (8 * sizeof (unsigned long int)))
+typedef struct
+  {
+    unsigned long int __val[_SIGSET_NWORDS];
+  } __sigset_t;
+
+/* from /usr/include/signal.h */
+typedef __sigset_t sigset_t;
 
 /* Aieee, gcc 2.95+ creates a stub for posix_types.h on i386 which brings
    glibc headers in and thus makes __FD_SET etc. not defined with 2.3+ kernels. */
@@ -38,29 +48,23 @@ extern int errno;
 #include <asm/unistd.h>
 #include <asm/fcntl.h>
 #include <asm/signal.h>
+#include <asm/stat.h>
+
+void * alloca(int size);
+void exit(int arg);
 
 #ifndef MINILIBC_INTERNAL
-
-#define X_OK	1
-#define W_OK	2
-#define R_OK	4
-
-#define S_IFBLK 0x6000
-
 static inline _syscall5(int,mount,const char *,spec,const char *,dir,const char *,type,unsigned long,rwflag,const void *,data);
 static inline _syscall5(int,_newselect,int,n,fd_set *,rd,fd_set *,wr,fd_set *,ex,struct timeval *,timeval);
 static inline _syscall4(int,wait4,pid_t,pid,int *,status,int,opts,void *,rusage)
 static inline _syscall3(int,write,int,fd,const char *,buf,unsigned long,count)
-static inline _syscall3(int,mknod,const char *,path,int,mode,int,dev)
 static inline _syscall3(int,reboot,int,magic,int,magic_too,int,flag)
 static inline _syscall3(int,execve,const char *,fn,void *,argv,void *,envp)
 static inline _syscall3(int,read,int,fd,const char *,buf,unsigned long,count)
 static inline _syscall3(int,open,const char *,fn,int,flags,mode_t,mode)
-static inline _syscall2(int,access,const char *,fn,int,flags)
 static inline _syscall3(int,ioctl,int,fd,int,request,void *,argp)
+static inline _syscall3(int,mknod,char *,path,int,mode,short,dev)
 static inline _syscall2(int,dup2,int,one,int,two)
-static inline _syscall2(int,mkdir,const char *,path,int,perms)
-static inline _syscall2(int,pivot_root,const char *,one,const char *,two)
 static inline _syscall2(int,kill,pid_t,pid,int,sig)
 static inline _syscall2(int,symlink,const char *,a,const char *,b)
 static inline _syscall2(int,chmod,const char * ,path,mode_t,mode)
@@ -68,10 +72,12 @@ static inline _syscall2(int,sethostname,const char *,name,int,len)
 static inline _syscall2(int,setdomainname,const char *,name,int,len)
 static inline _syscall2(int,setpgid,int,name,int,len)
 static inline _syscall2(int,signal,int,num,void *,len)
+static inline _syscall2(int,stat,const char *,file,struct stat *,buf)
 static inline _syscall1(int,umount,const char *,dir)
 static inline _syscall1(int,unlink,const char *,fn)
 static inline _syscall1(int,close,int,fd)
 static inline _syscall1(int,swapoff,const char *,fn)
+static inline _syscall1(int,umask,int,mask)
 static inline _syscall0(int,getpid)
 static inline _syscall0(int,getppid)
 static inline _syscall0(int,sync)
@@ -120,13 +126,12 @@ inline int accept(int a, void * addr, void * addr2);
 int strlen(const char * string);
 char * strcpy(char * dst, const char * src);
 void * memcpy(void * dst, const void * src, size_t count);
-void * memset(void * dst, int ch, size_t count);
 void sleep(int secs);
 int strcmp(const char * a, const char * b);
 int strncmp(const char * a, const char * b, int len);
 void printint(int i);
 void printf(char * fmt, ...);
-char * strchr(const char * str, int ch);
+char * strchr(char * str, int ch);
 char * strncpy(char * dst, const char * src, int len);
-void exit(int rc);
-int atoi(const char * str);
+
+void printstr(char * string);
