404ede6bb3ddf8e46393c5b1d0df91812ffebc25
Erik Troan <ewt@redhat.com>
2002-05-22 19:45:17 +0000

    added default/fallback tests

--- grubby/test.sh
+++ grubby/test.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-echo "Parse/write comparison..."
-for n in test/grub.[0-9]*; do 
-    ./grubby --remove-kernel 1234 -c $n -o - | cmp $n; 
+oneTest () {
+    cfg=test/$1
+    correct=test/$2
+    shift; shift
+    ./grubby --bad-image-okay -c $cfg -o - $* | cmp $correct > /dev/null
+
     if [ $? != 0 ]; then 
-	./grubby --remove-kernel 1234 -c $n -o - | diff -u - $n; 
+	echo -------------------------------------------------------------
+	echo FAILURE: $cfg $correct $*
+	./grubby --bad-image-okay -c $cfg -o - $* | diff -u $correct -; 
     fi 
+}
+
+echo "Parse/write comparison..."
+for n in $(cd test; echo grub.[0-9]*); do
+    oneTest $n $n --remove-kernel 1234
 done
+
 echo "Permission preservation..."
 cp test/grub.1 grub-test
 chmod 0614 grub-test 
@@ -16,3 +27,21 @@ if [  $p != '-rw---xr--' ]; then
     echo "  failed ($p)"; 
 fi
 rm -f grub-test
+
+echo "Default directive..."
+oneTest grub.1 results/default/1.1 --boot-filesystem=/boot --add-kernel /boot/new-kernel --title Some_Title 
+oneTest grub.1 results/default/1.2 --boot-filesystem=/boot --add-kernel /boot/new-kernel --title Some_Title --make-default
+oneTest grub.3 results/default/3.1 --boot-filesystem=/boot --set-default=/boot/vmlinuz-2.4.7-2
+oneTest grub.3 results/default/3.2 --boot-filesystem=/boot --set-default=/boot/vmlinuz-2.4.7-2smp
+oneTest grub.4 results/default/4.1 --boot-filesystem=/ --set-default=/boot/vmlinuz-2.4.7-ac3 --remove-kernel /boot/vmlinuz-2.4.7-2.5
+oneTest grub.4 results/default/4.2 --boot-filesystem=/ --set-default=/boot/vmlinuz-2.4.7-ac3 --remove-kernel /boot/vmlinuz-2.4.7-2.5 --add-kernel=/boot/new-kernel --copy-default --title New_Title
+oneTest grub.6 results/default/6.1 --remove-kernel=/boot/vmlinuz-2.4.7-2.9 --boot-filesystem=/
+
+echo "GRUB fallback directive..."
+oneTest grub.5 results/fallback/5.1 --remove-kernel=/boot/vmlinuz-2.4.7-ac3 \
+    --boot-filesystem=/
+oneTest grub.5 results/fallback/5.2 --remove-kernel=/boot/vmlinuz-2.4.7-2.5 \
+    --boot-filesystem=/
+oneTest grub.5 results/fallback/5.3 --remove-kernel=/boot/vmlinuz-2.4.7-2.5 \
+    --boot-filesystem=/ --copy-default --add-kernel=/boot/new-kernel \
+    --title="Some_Title"
