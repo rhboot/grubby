fe49f22c77a5284224b90cee3973dda69831e6b7
Peter Jones <pjones@redhat.com>
2005-03-15 15:37:15 +0000

    Fix accidental formatting of data in grub.conf (#151118, from proski@gnu.org)

--- grubby.c
+++ grubby.c
@@ -386,14 +386,14 @@ static int lineWrite(FILE * out, struct singleLine * line,
 		     struct configFileInfo * cfi) {
     int i;
 
-    if (fprintf(out, line->indent) == -1) return -1;
+    if (fprintf(out, "%s", line->indent) == -1) return -1;
 
     for (i = 0; i < line->numElements; i++) {
 	if (i == 1 && line->type == LT_KERNELARGS && cfi->argsInQuotes)
 	    if (fputc('"', out) == EOF) return -1;
 
-	if (fprintf(out, line->elements[i].item) == -1) return -1;
-	if (fprintf(out, line->elements[i].indent) == -1) return -1;
+	if (fprintf(out, "%s", line->elements[i].item) == -1) return -1;
+	if (fprintf(out, "%s", line->elements[i].indent) == -1) return -1;
     }
 
     if (line->type == LT_KERNELARGS && cfi->argsInQuotes)
