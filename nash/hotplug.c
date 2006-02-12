45d024164311b1e825a85dc9c555e0b6d551e404
Peter Jones <pjones@redhat.com>
2006-02-12 21:23:42 +0000

    - basic hotplug handler/firmware loader

---
+++ nash/hotplug.c
@@ -0,0 +1,487 @@
+/*
+* hotplug.c -- firmware loading/hotplug event handling
+*
+* Peter Jones (pjones@redhat.com)
+*
+* Copyright 2006 Red Hat, Inc.
+*
+* This software may be freely redistributed under the terms of the GNU
+* public license.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program; if not, write to the Free Software
+* Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*
+* vim:ts=8:sw=4:sts=4:et
+*/
+
+#define _GNU_SOURCE 1
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <sys/time.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <signal.h>
+
+#include "hotplug.h"
+#include "lib.h"
+
+#include <asm/types.h>
+#include <linux/netlink.h>
+
+#ifndef NETLINK_KOBJECT_UEVENT
+#define NETLINK_KOBJECT_UEVENT 15
+#endif
+
+static void
+udelay(int usecs)
+{
+    struct timespec req = {
+        .tv_sec = 0,
+        .tv_nsec = usecs * 100,
+    };
+    struct timespec rem = {
+        .tv_sec = 0,
+        .tv_nsec = 0,
+    };
+
+    while (nanosleep(&req, &rem) < 0 && errno == EINTR) {
+        errno = 0;
+        req.tv_sec = rem.tv_sec;
+        rem.tv_sec = 0;
+        req.tv_nsec = rem.tv_nsec;
+        rem.tv_nsec = 0;
+    }
+}
+
+/* Set the 'loading' attribute for a firmware device.
+ * 1 == currently loading
+ * 0 == done loading
+ * -1 == error
+ */
+static int
+set_loading(int fd, const char *device, int value) {
+    char buf[10] = {'\0'};
+    int rc;
+
+    if (fd < 0) {
+        char loading_path[1024];
+
+        snprintf(loading_path, sizeof(loading_path), "/sys/%s/loading", device);
+        loading_path[sizeof(loading_path)-1] = '\0';
+        fd = coeOpen(loading_path, O_RDWR | O_NONBLOCK | O_SYNC );
+        if (fd < 0)
+            return fd;
+    }
+    if ((rc = snprintf(buf, 9, "%d", value)) < 0 ||
+            (rc = write(fd, buf, strlen(buf) + 1)) < 0) {
+        close(fd);
+        return rc;
+    }
+    return fd;
+}
+
+static int
+file_map(const char *filename, char **buf, size_t *bufsize)
+{
+    struct stat stats;
+    int fd;
+
+    fd = coeOpen(filename, O_RDONLY);
+    if (fd < 0) {
+        return -1;
+    }
+
+    if (fstat(fd, &stats) < 0) {
+        close(fd);
+        return -1;
+    }
+
+    *buf = mmap(NULL, stats.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    if (*buf == MAP_FAILED) {
+        close(fd);
+        return -1;
+    }
+    *bufsize = stats.st_size;
+
+    close(fd);
+
+    return 0;
+}
+
+static void
+file_unmap(void *buf, size_t bufsize)
+{
+    munmap(buf, bufsize);
+}
+
+static void
+load_firmware(void)
+{
+    char *physdevbus = NULL, *physdevdriver = NULL, *physdevpath = NULL,
+         *devpath = NULL, *firmware = NULL;
+    char fw[1024] = "/lib/firmware/", driver[1024] = "/sys/bus/", data[1024] = "/sys";
+    int dfd=-1, lfd=-1;
+    int timeout = 0, loading = -2;
+    char *fw_buf = NULL;
+    size_t fw_len=0;
+    size_t count;
+
+    devpath = getenv("DEVPATH");
+    firmware = getenv("FIRMWARE");
+    physdevbus = getenv("PHYSDEVBUS");
+    physdevdriver = getenv("PHYSDEVDRIVER");
+    physdevpath = getenv("PHYSDEVPATH");
+    
+    if (!devpath || !firmware || !physdevbus || !physdevdriver || !physdevpath) {
+        fprintf(stderr, "couldn't get environment\n");
+        fprintf(stderr, "%s: %s\n", "DEVPATH", devpath);
+        fprintf(stderr, "%s: %s\n", "FIRMWARE", firmware);
+        fprintf(stderr, "%s: %s\n", "PHYSDEVBUS", physdevbus);
+        fprintf(stderr, "%s: %s\n", "PHYSDEVDRIVER", physdevdriver);
+        fprintf(stderr, "%s: %s\n", "PHYSDEVPATH", physdevpath);
+        return;
+    }
+
+    driver[9] = '\0';
+    strcat(driver, physdevbus);
+    strcat(driver, "/drivers/");
+    strcat(driver, physdevdriver);
+    while (access(driver, F_OK)) {
+        fprintf(stderr, "waiting for %s\n", driver);
+        udelay(100);
+    }
+
+    lfd = set_loading(lfd, devpath, 1);
+
+    fw[14] = '\0';
+    strcat(fw, firmware);
+    if (file_map(fw, &fw_buf, &fw_len) < 0) {
+        fw_buf = NULL;
+        loading = -2;
+        goto out;
+    }
+
+    data[4] = '\0';
+    strcat(data, devpath);
+    strcat(data, "/data");
+    dfd = coeOpen(data, O_RDWR);
+    if (dfd < 0) {
+        fprintf(stderr, "failed to open %s\n", data);
+        loading = -1;
+        goto out;
+    }
+    count = 0;
+    while (count < fw_len) {
+        ssize_t c;
+        c = write(dfd, fw_buf + count, fw_len - count);
+        if (c <= 0) {
+            loading = -1;
+            goto out;
+        }
+        count += c;
+    }
+    loading = 0;
+
+out:
+    if (timeout)
+        fprintf(stderr, "timeout loading %s\n", firmware);
+
+    if (dfd >= 0)
+        close(dfd);
+    if (fw_buf)
+        file_unmap(fw_buf, fw_len);
+    if (loading != -2)
+        lfd = set_loading(lfd, devpath, loading);
+    if (lfd != -1)
+        close(lfd);
+
+    return;
+}
+
+static int
+get_netlink_msg(int fd, char **msg, char **path)
+{
+    size_t len;
+    ssize_t size;
+    static char buffer[2560];
+    char *pos;
+
+    size = recv(fd, &buffer, sizeof (buffer), 0);
+    if (size < 0)
+        return -1;
+
+    if ((size_t)size > sizeof (buffer) - 1)
+        size = sizeof (buffer) - 1;
+    buffer[size] = '\0';
+
+    len = strcspn(buffer, "@");
+    if (!buffer[len])
+        return -1;
+
+    pos = buffer;
+    *msg = strndup(pos, len++);
+    pos += len;
+    *path = strdup(pos);
+
+    pos += strlen(pos) + 1;
+    if (len < size + 1) {
+        clearenv();
+        putenv("MALLOC_PERTURB_=204");
+
+        while (pos[0]) {
+            putenv(pos);
+            pos += strlen(pos) + 1;
+        }
+    }
+    return 0;
+}
+
+static void
+handle_events(int exitfd, int nlfd)
+{
+    fd_set fds;
+    int maxfd = nlfd;
+    int fdcount;
+    int doexit = 0;
+    typedef enum states {
+        IDLE,
+        HANDLE_FIRMWARE_ADD,
+        HANDLE_FIRMWARE_REMOVE,
+        END_OF_LIST,
+    } states;
+    states state = IDLE;
+    char *token;
+    long prev = 0;
+
+    FD_ZERO(&fds);
+    if (exitfd > 0) {
+        if (exitfd > nlfd)
+            maxfd = exitfd;
+        FD_SET(exitfd, &fds);
+    }
+    FD_SET(nlfd, &fds);
+
+
+    do {
+        fdcount = select(maxfd+1, &fds, NULL, NULL, NULL);
+        if (fdcount < 0) {
+            if (errno == EINVAL)
+                exit(1);
+            continue;
+        }
+
+        if (FD_ISSET(nlfd, &fds)) {
+            char *action = NULL, *subsystem = NULL, *seqnum = NULL;
+            long cur;
+            char *msg = NULL, *path = NULL;
+            int done = 0;
+
+            if (get_netlink_msg(nlfd, &msg, &path) < 0)
+                goto testexit;
+                
+            assert(msg);
+            assert(path);
+
+            action = getenv("ACTION");
+            subsystem = getenv("SUBSYSTEM");
+            seqnum = getenv("SEQNUM");
+
+            if (!action || !subsystem || !seqnum) {
+                fprintf(stderr, "couldn't get environment\n");
+                fprintf(stderr, "ACTION=\"%s\" SUBSYSTEM=\"%s\" SEQNUM=\"%s\"\n",
+                        action, subsystem, seqnum);
+                return;
+            }
+            assert(subsystem);
+            assert(action);
+            assert(seqnum);
+
+            cur = strtol(seqnum, NULL, 0);
+            if (cur < prev)
+                fprintf(stderr, "WARNING: events out of order: %ld -> %ld\n", prev, cur);
+            if (cur == prev)
+                fprintf(stderr, "WARNING: duplicate event %ld\n", cur);
+            prev = cur;
+
+            while (!done) switch (state) {
+                case IDLE:
+                    if (!strcmp(action, "add") && !strcmp(subsystem, "firmware")) {
+                        state = HANDLE_FIRMWARE_ADD;
+                        token=strdup(getenv("FIRMWARE"));
+                        load_firmware();
+                    } else {
+                        //fprintf(stderr, "unkown action %s %s\n", action, subsystem);
+                    }
+                    done = 1;
+                    break;
+                case HANDLE_FIRMWARE_ADD:
+                    if (!strcmp(msg, "remove") && !strcmp(subsystem, "firmware")) {
+                        if (strcmp(token, getenv("FIRMWARE")))
+                            fprintf(stderr, "WARNING: add firmware %s followed by remove firmware %s\n", token, getenv("FIRMWARE"));
+                        free(token);
+                        token = NULL;
+                        state = HANDLE_FIRMWARE_REMOVE;
+                    } else {
+                        //fprintf(stderr, "unkown action %s %s\n", action, subsystem);
+                        state = IDLE;
+                        done = 1;
+                    }
+                    break;
+                case HANDLE_FIRMWARE_REMOVE:
+                default:
+                    state = IDLE;
+                    done = 1;
+                    break;
+            }
+            free(msg);
+            free(path);
+        }
+
+testexit:
+        if (exitfd > 0 && FD_ISSET(exitfd, &fds)) {
+            char buf[9];
+
+            read(exitfd, &buf, 13);
+            if (!strcmp(buf, "die udev die"))
+                doexit=1;
+            continue;
+        }
+        fprintf(stderr, "should never have gotten here!\n");
+        assert(0);
+    } while (!doexit);
+}
+
+static int pipefd = -1;
+
+void
+kill_hotplug(void) {
+    if (pipefd > 0) {
+        write(pipefd, "die udev die", 13);
+        fsync(pipefd);
+    }
+    exit(0);
+}
+
+#ifdef FWDEBUG
+static void
+kill_hotplug_signal(int signal) {
+    kill_hotplug();
+}
+#endif
+
+static int
+daemonize(int pipefds[2])
+{
+    int i;
+    int netlink;
+    struct sockaddr_nl sa;
+
+    netlink = socket(PF_NETLINK, SOCK_DGRAM, NETLINK_KOBJECT_UEVENT);
+    if (netlink < 0) {
+        fprintf(stderr, "could not open netlink socket: %m\n");
+        close(pipefds[0]);
+        close(pipefds[1]);
+        return -1;
+    }
+    makeFdCoe(netlink);
+
+    memset(&sa, '\0', sizeof (sa));
+    sa.nl_family = AF_NETLINK;
+    sa.nl_pid = getpid();
+    sa.nl_groups = -1;
+
+    if (bind(netlink, (struct sockaddr *)&sa, sizeof (sa)) < 0) {
+        fprintf(stderr, "could not bind to netlink socket: %m\n");
+        close(netlink);
+        close(pipefds[0]);
+        close(pipefds[1]);
+        return -1;
+    }
+
+    if (fork() > 0) {
+        close(pipefds[0]);
+        close(netlink);
+
+        pipefd = pipefds[1];
+#ifdef FWDEBUG
+        signal(SIGINT, kill_hotplug_signal);
+#endif
+        return 0;
+    }
+    close(pipefds[1]);
+
+#ifndef FWDEBUG
+    for (i = 0; i < getdtablesize(); i++) {
+        if (i != pipefds[1] && i != netlink)
+            close(i);
+    }
+
+    for (i = 0; i <= 2; i++) {
+        int fd;
+
+        close(i);
+        if (i == 0)
+            fd = coeOpen("/dev/zero", O_RDONLY);
+        else
+            fd = coeOpen("/dev/null", O_RDWR);
+        if (fd != i) {
+            dup2(fd, i);
+            close(fd);
+        }
+    }
+
+    setsid();
+
+    chdir("/");
+
+    signal(SIGTTOU, SIG_IGN);
+    signal(SIGTTIN, SIG_IGN);
+    signal(SIGTSTP, SIG_IGN);
+#endif
+    signal(SIGINT, SIG_IGN);
+
+    i = coeOpen("/proc/self/oom_adj", O_RDWR);
+    if (i >= 0) {
+        write(i, "-17", 3);
+        close(i);
+    }
+
+    handle_events(pipefds[0], netlink);
+    exit(0);
+}
+
+int 
+init_hotplug(void) {
+    int filedes[2] = {-1,-1};
+    
+    if (pipe(filedes) < 0) {
+        fprintf(stderr, "error: %m\n");
+        return 1;
+    }
+    makeFdCoe(filedes[0]);
+    makeFdCoe(filedes[1]);
+
+    if (daemonize(filedes) < 0)
+        return 1;
+    return 0;
+}
+
+#ifdef FWDEBUG
+int main(void) {
+    putenv("MALLOC_PERTURB_=204");
+    init_hotplug();
+    while (sleep(86400) > 0)
+        ;
+    return 0;
+}
+#endif
