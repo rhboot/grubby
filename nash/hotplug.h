59e515782da59bc4c5efd360c3d02379acec4a24
Peter Jones <pjones@redhat.com>
2006-02-12 21:09:49 +0000

    - define functions to start and stop hotplug

---
+++ nash/hotplug.h
@@ -0,0 +1,24 @@
+/*
+ * hotplug.h -- hotplug loading/hotplug event handling
+ *
+ * Peter Jones (pjones@redhat.com)
+ *
+ * Copyright 2006 Red Hat, Inc.
+ *
+ * This software may be freely redistributed under the terms of the GNU
+ * public license.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * vim:ts=8:sw=4:sts=4:et
+ */
+
+#ifndef NASH_HOTPLUG_H
+#define NASH_HOTPLUG_H 1
+
+extern int init_hotplug(void);
+extern void kill_hotplug(void);
+
+#endif /* NASH_HOTPLUG_H */
