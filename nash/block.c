eb17cfe28a03d9771cde452fb762f21a7aa1b3a9
Peter Jones <pjones@redhat.com>
2006-02-14 23:03:54 +0000

    - don't probe floppies

--- nash/block.c
+++ nash/block.c
@@ -166,7 +166,13 @@ block_sysfs_try_dir(bdev_iter iter, char *sysfs_path, bdev *dev)
     int ret;
     dev_t devno;
 
+    if (!strncmp(iter->dent->d_name, "fd", 2))
+        return -1;
+
     ret = parse_sysfs_devnum(sysfs_path, &devno);
+    /* don't probe floppies */
+    if (!strncmp(iter->dent->d_name, "fd", 2) && major(devno) == 2)
+        return -1;
     if (ret == 0) {
         /* we can't just assign it to *dev,
            or gcc decides it's unused */
