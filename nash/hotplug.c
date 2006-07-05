6c1a9b85c93f41f64916f264078ff3eb95251118
Peter Jones <pjones@redhat.com>
2006-07-05 21:38:37 +0000

    - this doesn't need two vim comments

--- nash/hotplug.c
+++ nash/hotplug.c
@@ -11,8 +11,6 @@
 * You should have received a copy of the GNU General Public License
 * along with this program; if not, write to the Free Software
 * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*
-* vim:ts=8:sw=4:sts=4:et
 */
 
 #define _GNU_SOURCE 1
