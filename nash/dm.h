de3755d0636c1dbfdd6658891b5c681992be8903
Peter Jones <pjones@redhat.com>
2006-07-05 20:56:04 +0000

    - rename NASH_DM_H to NASH_PRIV_DM_H

--- nash/dm.h
+++ nash/dm.h
@@ -15,8 +15,8 @@
  * vim:ts=8:sw=4:sts=4:et
  */
 
-#ifndef NASH_DM_H
-#define NASH_DM_H 1
+#ifndef NASH_PRIV_DM_H
+#define NASH_PRIV_DM_H 1
 
 extern char *
 nashDmGetUUID(const char *name);
@@ -48,4 +48,4 @@ nashDmFreeTree(void);
 extern void
 dm_cleanup(void);
 
-#endif /* NASH_DM_H */
+#endif /* NASH_PRIV_DM_H */
