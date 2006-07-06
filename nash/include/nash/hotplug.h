cc77a1aee6e19c0291ce4cfe58f54a9e7943d911
Peter Jones <pjones@redhat.com>
2006-07-06 20:09:03 +0000

    - add nashFileFetcher_t and get/set functions - add get/set for firmware search path

--- nash/include/nash/hotplug.h
+++ nash/include/nash/hotplug.h
@@ -23,6 +23,11 @@ extern void nashHotplugNewRoot(struct nash_context *);
 extern void nashHotplugNotifyExit(struct nash_context *);
 extern void nashHotplugKill(struct nash_context *);
 
-extern int nashSetFirmwareDir(char *dir);
+typedef int (*nashFileFetcher_t)(char *inpath, int outfd);
+
+extern void nashSetFileFetcher(struct nash_context *, nashFileFetcher_t);
+extern nashFileFetcher_t nashGetFileFetcher(struct nash_context *);
+extern int nashSetFirmwarePath(struct nash_context *, char *);
+extern char *nashGetFirmwarePath(struct nash_context *);
 
 #endif /* NASH_HOTPLUG_H */
