d1c92afe717417328d71ae347620203c8abdb37c
Peter Jones <pjones@redhat.com>
2006-11-27 22:29:36 +0000

    - Test for _GNU_SOURCE before setting it.

--- grubby/grubby.c
+++ grubby/grubby.c
@@ -15,7 +15,9 @@
    Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
    02111-1307 USA.  */
 
+#ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#endif
 #include <ctype.h>
 #include <errno.h>
 #include <fcntl.h>
