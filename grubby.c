ae5490b72e6de39d5876c4fe0734bb3ca62408c0
Lubomir Rintel <lkundrak@v3.sk>
2013-11-27 16:59:50 +0100

    extlinux: Understand "default" properly
    The configuration looks like the following:
    
      default Fedora (3.11.6-301.fc20.i686+PAE) 20 (Heisenbug)
    
      title Fedora (3.11.6-301.fc20.i686+PAE) 20 (Heisenbug)
      ...
    
    Grubby skips over the default clause as it has more than one element. And even
    if it did not, it would not match it against the title, since it handles titles
    specially, concatenating the title, but not the default clause.
    
    This commit adds special handling for extlinux, which causes it to parse
    default and title in the same way.
    
    Signed-off-by: Lubomir Rintel <lkundrak@v3.sk>

--- grubby.c
+++ grubby.c
@@ -161,6 +161,7 @@ struct configFileInfo {
     int defaultIsVariable;
     int defaultSupportSaved;
     int defaultIsSaved;
+    int defaultIsUnquoted;
     enum lineType_e entryStart;
     enum lineType_e entryEnd;
     int needsBootPrefix;
@@ -635,6 +636,7 @@ struct configFileInfo extlinuxConfigType = {
     .needsBootPrefix = 1,
     .maxTitleLength = 255,
     .mbAllowExtraInitRds = 1,
+    .defaultIsUnquoted = 1,
 };
 
 struct grubConfig {
@@ -1190,9 +1192,6 @@ static struct grubConfig * readConfig(const char * inName,
 		cfg->flags &= ~GRUB_CONFIG_NO_DEFAULT;
 		defaultLine = line;
 	    }
-	} else if (line->type == LT_DEFAULT && line->numElements == 2) {
-	    cfg->flags &= ~GRUB_CONFIG_NO_DEFAULT;
-	    defaultLine = line;
 
         } else if (iskernel(line->type)) {
 	    /* if by some freak chance this is multiboot and the "module"
@@ -1225,8 +1224,9 @@ static struct grubConfig * readConfig(const char * inName,
 	    cfg->fallbackImage = strtol(line->elements[1].item, &end, 10);
 	    if (*end) cfg->fallbackImage = -1;
 
-	} else if (line->type == LT_TITLE && line->numElements > 1) {
-	    /* make the title a single argument (undoing our parsing) */
+	} else if ((line->type == LT_DEFAULT && cfi->defaultIsUnquoted) ||
+                (line->type == LT_TITLE && line->numElements > 1)) {
+	    /* make the title/default a single argument (undoing our parsing) */
 	    len = 0;
 	    for (int i = 1; i < line->numElements; i++) {
 		len += strlen(line->elements[i].item);
@@ -1331,6 +1331,11 @@ static struct grubConfig * readConfig(const char * inName,
 		if (isquote(line->elements[last].item[len]))
 		    line->elements[last].item[len] = '\0';
 	    }
+	}
+
+	if (line->type == LT_DEFAULT && line->numElements == 2) {
+	    cfg->flags &= ~GRUB_CONFIG_NO_DEFAULT;
+	    defaultLine = line;
 	}
 
 	/* If we find a generic config option which should live at the
