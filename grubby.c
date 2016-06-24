d4d7954a983f9cf68cd49c181e8deb1493e76e9e
Peter Jones <pjones@redhat.com>
2016-06-24 16:05:45 -0400

    Ensure command line updates also honor --set-index
    Related: rhbz#1285601
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- grubby.c
+++ grubby.c
@@ -4292,10 +4292,16 @@ int addNewKernel(struct grubConfig *config, struct singleEntry *template,
 	struct singleEntry *new, *entry, *prev = NULL;
 	struct singleLine *newLine = NULL, *tmplLine = NULL, *masterLine = NULL;
 	int needs;
+	char *indexs;
 	char *chptr;
+	int rc;
 
 	if (!newKernelPath)
 		return 0;
+
+	rc = asprintf(&indexs, "%d", newIndex);
+	if (rc < 0)
+		return 1;
 
 	/* if the newKernelTitle is too long silently munge it into something
 	 * we can live with. truncating is first check, then we'll just mess with
@@ -4753,7 +4759,7 @@ int addNewKernel(struct grubConfig *config, struct singleEntry *template,
 		abort();
 	}
 
-	if (updateImage(config, "0", prefix, newKernelArgs, NULL,
+	if (updateImage(config, indexs, prefix, newKernelArgs, NULL,
 			newMBKernelArgs, NULL))
 		return 1;
 
